2d089aefd252f4735797f3571ff4eec3bf3c3e20
Anita Kumari <anitavkapari@gmail.com>
2018-07-28 22:04:52 +0530

    Updated

--- app/src/main/java/com/example/annu/recyclerview/MainActivity.java
+++ app/src/main/java/com/example/annu/recyclerview/MainActivity.java
@@ -22,18 +22,32 @@ RecyclerView rvEmployee;
 //create list
         List<Employee> employeeList= new ArrayList<>();
         //adding data list
-        employeeList.add(new Employee(R.drawable.ic_launcher_background,
-                "Ravi","Saswad","28988978"));
+        employeeList.add(new Employee(R.drawable.dad,
+                "Dad","Simri","9122495172"));
+        employeeList.add(new Employee(R.drawable.mummy,
+                "Mummy","Simri","8862938816"));
+        employeeList.add(new Employee(R.drawable.ic_person_outline_black_24dp,
+                "Myno.","Saswad","9763075433"));
         employeeList.add(new Employee(R.drawable.profile,
-                "Deversh","Pune","9883782833"));
-        employeeList.add(new Employee(R.drawable.ic_launcher_foreground,
-                "Deversh","Pune","9883782833"));
-        employeeList.add(new Employee(R.drawable.profile,
-                "prem","Mumbai","6171888"));
-        employeeList.add(new Employee(R.drawable.ic_launcher_foreground,
-                "prem","Mumbai","6171888"));
-        employeeList.add(new Employee(R.drawable.profile,
-                "prem","Mumbai","6171888"));
+                "Vikash jee","Saswad","9881078842"));
+        employeeList.add(new Employee(R.drawable.kanchandee,
+                "Kanchan Dee","Kolkata","8596835821"));
+        employeeList.add(new Employee(R.drawable.kavita,
+                "Kavita","Sitamadhi","7979716310"));
+        employeeList.add(new Employee(R.drawable.rohit,
+                "Rohit jee","Sitamadhi","9102697817"));
+        employeeList.add(new Employee(R.drawable.uday,
+                "Uday jee","Delhi","8435002994"));
+        employeeList.add(new Employee(R.drawable.guddu,
+                "Roushan","Delhi","7814245468"));
+        employeeList.add(new Employee(R.drawable.buaaji,
+                "BuaaJee","pune","8830304705"));
+        employeeList.add(new Employee(R.drawable.papa,
+                "Papa","Umgaon","8210975982"));
+        employeeList.add(new Employee(R.drawable.bhai,
+                "Bhai","Gurgaon","9999149690"));
+        employeeList.add(new Employee(R.drawable.bhabhi,
+                "Bhabhi","Gurgaon","9999149590"));
         //create adapter
         EmployeeAdapter employeeAdapter = new EmployeeAdapter(employeeList);
 
